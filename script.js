36e4e1945178c009cf49936d7f55545bf2e79880
m13ks <korzhanm@gmail.com>
2023-03-02 14:04:12 +0200

    add logic operators

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-let firstNumber = prompt ('Введіть будь яке число');
-let secondNumber = prompt ('Введіть будь яке число, окрім нуля!!!');
-
-let a = Number(firstNumber);
-let b = Number(secondNumber);
-
-let sum = a + b;
-let diff = a - b;
-let prod = a * b;
-let prop = a / b;
+const firstNumber = prompt ('Введіть будь яке число');
+const secondNumber = prompt ('Введіть будь яке число');
 
 
-alert (`Сума цих чисел: ${sum}
-Різниця цих чисел: ${diff}
-Добуток цих чисел: ${prod}
-Часта цих чисел: ${prop}`);
+if (firstNumber == "" || secondNumber == "") {
+    alert ('Ви пропустили ввід числа');
+} else{
+    let a = Number(firstNumber);
+    let b = Number(secondNumber);
+    if (b == 0) {
+        alert ('На нуль ділити не можна!');
+    } else {
+        let sum = a + b;
+        let diff = a - b;
+        let prod = a * b;
+        let prop = a / b;
+    
+        const result = `Сума цих чисел: ${sum}
+        Різниця цих чисел: ${diff}
+        Добуток цих чисел: ${prod}
+        Часта цих чисел: ${prop}`
+    
+        if (a < b) {
+            let difference = confirm('Ви впевнені, що хочете продовжити операцію?');
+            if(difference) {
+            alert(result);
+            }
+        } else {
+            alert(result);
+        }
+            
+    }
+
+}
+
+
+
+
